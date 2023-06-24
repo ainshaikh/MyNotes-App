ad79b7abb98a0d7676fbc8783e217c64e8357178
Ain Shaikh <ainsaikhx7@gmail.com>
2023-06-24 11:04:05 +0530

    connection to mongoDB made

---
+++ backened/db.js
@@ -0,0 +1,13 @@
+const mongoose = require ('mongoose');
+const mongoURI = 'mongodb://0.0.0.0:27017';
+
+const connectToMongo = async () => {
+    try {
+      await mongoose.connect(mongoURI);
+      console.log('Connected to MongoDB successfully');
+    } catch (error) {
+      console.error('Failed to connect to MongoDB:', error);
+    }
+  };
+
+module.exports = connectToMongo;

e3b06ce8e6e80f5430dbfd58bfde556ecf5a6b4c
Ain Shaikh <ainsaikhx7@gmail.com>
2023-06-24 14:12:00 +0530

    :rocket:created models for schema and routes

--- backened/index.js
+++ backened/index.js
@@ -5,9 +5,15 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Ain')
-})
+app.use(express.json())
+
+//Availble Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Ain')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

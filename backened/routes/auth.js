cb2561149f671f2a2482de8f57a44971c1a20fd2
Ain Shaikh <ainsaikhx7@gmail.com>
2023-06-26 17:09:12 +0530

    :rocket:login endpoint added

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -44,7 +44,7 @@ router.post(
         email: req.body.email,
         password: secretPassword,
       });
-
+      // Token authentication with JWT
       const data = {
         user:{
             id: user.id
@@ -59,6 +59,50 @@ router.post(
         res.status(500).send("some error occured")
     }
     }  );  
-  
+
+
+    // Authenticate  a user using POST "api/auth/login".
+    router.post(
+        "/login",
+        [
+          // Validation of email, name and password
+          
+          body("email").isEmail().withMessage("Enter an email address"),
+          body("password")
+            .isLength({ min: 5 })
+            .withMessage("Password cannot be empty"),
+        ],
+        async (req, res) => {
+         
+            // if there are errors, return bad request with errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+            }
+            const {email, password} = req.body;
+            try{
+                let user = await User.findOne({email});
+                if(!user){
+                    return res.status(400).json({error: "please write correct credentials"})
+                }
+                const passwordCompare = await bcrypt.compare(password, user.password);
+                if(!passwordCompare){
+                    return res.status(400).json({error: "please write correct credentials"})
+                }
+                const data = {
+                    user:{
+                        id: user.id
+                    }
+                  }
+                  var authToken = jwt.sign({data}, JWD_secret);
+            
+                            res.json({authToken});
+            }
+            catch(err){
+                console.log(err.message);
+                res.status(500).send("Internal server Error")
+            }
+        })
+    
 
 module.exports = router;
